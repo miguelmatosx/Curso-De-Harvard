c94c8355419a9cedd7f560848db454ca8e475579
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-19 00:35:24 +0000

    Thu, Dec 18, 2025, 8:35 PM -04:00

--- agree.c
+++ agree.c
@@ -1,2 +1,15 @@
 #include<stdio.h>
 #include<cs50.h>
+
+int main (void)
+{
+    char c = get_char("Do you agree?");
+    if( c="y")
+    {
+        printf("Agreed.\n")
+    }
+    else if (c ="n")
+    {
+        printf("Not Agreed\n");
+    }
+}
