c898336bfde1954aee9632f65947995d18ee5995
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-19 00:16:31 +0000

    Thu, Dec 18, 2025, 8:16 PM -04:00

--- truncation.c
+++ truncation.c
@@ -6,8 +6,17 @@ int main(void)
     int x = get_int( "x:  ");
     int y = get_int ("y:  ");
 
-    float z = (float) x/(float) y;
-
-    printf("%f\n", z);
-
+    if ( x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else if (x>y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
 }
+
