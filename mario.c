4c8a482c0e08a588bc9fe73a89c58642481329b7
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-22 15:04:29 +0000

    Mon, Dec 22, 2025, 11:04 AM -04:00

--- mario.c
+++ mario.c
@@ -3,17 +3,30 @@
 
 int main(void)
 {
-    int n = get_int("Size: ");
-    while (n > 1)
+    int n;
+
+    // Pede o número até ser válido
+    do
     {
         n = get_int("Size: ");
     }
+    while (n < 1 || n > 8);
 
+    // Constrói a pirâmide
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        // Espaços
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            printf(" ");
+        }
 
+        // #
+        for (int j = 0; j < i + 1; j++)
+        {
             printf("#");
+        }
+
         printf("\n");
     }
 }
