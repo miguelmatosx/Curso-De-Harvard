42001719d08d3418ef3b483958136c8244f1885a
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-22 13:20:21 +0000

    Mon, Dec 22, 2025, 9:20 AM -04:00

--- mario.c
+++ mario.c
@@ -15,3 +15,13 @@ int main (void)
 
    printf("#\n");
 }
+   for( int i=0 ; i<3 ; i++)
+   {
+      for( int j = 0 ; j<3 ; j++)
+      {
+         printf("#");
+      }
+      printf("\n");
+   }
+
+}
