b83f23c18c06355a307ccefb511ab8efb9e35d41
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-22 14:09:35 +0000

    Mon, Dec 22, 2025, 10:09 AM -04:00

--- mario.c
+++ mario.c
@@ -3,19 +3,8 @@
 
 int main(void)
 {
-    int n= get_int(" Size: ");
-    while (get_int( Size:  ))
+    printf("#"\n);
 
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j< n ; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
-}
 
 
 
