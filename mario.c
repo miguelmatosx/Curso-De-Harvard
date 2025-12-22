e8eb0d4b46d74cce36e3ac9a364279c7f2121906
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-22 13:41:18 +0000

    Mon, Dec 22, 2025, 9:41 AM -04:00

--- mario.c
+++ mario.c
@@ -1,24 +1,23 @@
-#include<stdio.h>
-#include<cs50.h>
+#include <cs50.h>
+#include <stdio.h>
 
-int main (void)
+int main(void)
 {
-   int n;
-   do
-   {
-      n= get_int("Size: ");
-   }
-   while( n < 1 || n > 8);
-   return n;
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
 
- for (int i= 0 ; i<n ; i++)
-   {
-      for (int j=0 ; j < n ; j++)
-      {
-         printf("#");
-      }
-      printf("\n");
-   }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
 
 
