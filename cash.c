f64c1da4190b6e6c948334c15556e550c4a6dfde
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-29 12:03:20 +0000

    Mon, Dec 29, 2026, 8:03 AM -04:00

--- cash.c
+++ cash.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
-#include < math.h>
+#include <math.h>
 
 
 int main (void)
 {
-   double Valor
-   int centavos
+   double Valor;
+   int centavos;
 
    do
    {
-    printf( "Qual valor inserido");
-    scanf (%lf, &valor) ;
+    printf( "Qual valor inserido:");
+    scanf ("%lf", &Valor) ;
    }
-   while( valor < 0);
+   while( Valor < 0);
 
    centavos = round(Valor * 100);
 
@@ -22,6 +22,15 @@ int main (void)
    moedas += centavos/25;
    centavos%= 25;
 
-   moedas += centavos/16
-   centavos%= 16
+   moedas += centavos/10;
+   centavos%= 10;
+
+   moedas += centavos/5;
+   centavos%= 5;
+
+   moedas += centavos;
+
+   printf("%d\n", moedas);
+
+   return 0;
 }
