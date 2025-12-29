fe146c33c924ee28ee8705e7ecc1591b768c8737
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-29 00:03:26 +0000

    Sun, Dec 28, 2025, 8:03 PM -04:00

--- mario2.c
+++ mario2.c
@@ -10,7 +10,7 @@ int main(void)
     }
     while( n < 1 || n > 8);
 
-    for (int i = 0 ; int i < n ; i++)
+    for (int i = 0 ; i < n ; i++)
     {
         for(int j = 0 ; j < n - i - 1 ; j++)
 
