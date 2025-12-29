5978284917f11f4d16c316a183314e6aa1645b61
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2025-12-29 22:10:14 +0000

    Mon, Dec 29, 2026, 6:10 PM -04:00

--- population.c
+++ population.c
@@ -3,12 +3,17 @@
 
 int main (void)
 {
-    int quani
-
+    int quani;
+    int quanf;
+    int Years;
     do
     {
-         quani= get_int("Quantidade Inicial?\n");
+         quani= get_int("Quantidade Inicial:\n");
+         quanf= get_int(" Quantidade Final:\n");
+         Years= get_int("Quantidade de anos:\n");
+
+
     }
-    while( quani >== 9);
-    
+    while( quani <= 9 ; quanf < 9 ; Years < 1);
+
 }
