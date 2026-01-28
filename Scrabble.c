924f830521f614fa375c87fa6ab04818472eab99
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-01-28 22:30:36 +0000

    Wed, Jan 28, 2026, 6:30 PM -04:00

--- Scrabble.c
+++ Scrabble.c
@@ -5,7 +5,7 @@
 
 int POINTS[]= {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int computer_score(string world);
+int compute_score(string world);
 
 int main(void)
 {
@@ -40,9 +40,16 @@ int main(void)
     {
         valido= false;
         break;
+
     }
 
   }
   while(!valido);
+
+
+  int compute_score(string word1)
+  {
+    int score= 0
+  }
 }
 
