fb976930063f1fdc1d6c6ed555b098685f02868c
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-01-30 14:32:54 +0000

    Fri, Jan 30, 2026, 10:32 AM -04:00

--- Scrabble.c
+++ Scrabble.c
@@ -5,51 +5,73 @@
 
 int POINTS[]= {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string world);
+
+int compute_score(string word);
 
 int main(void)
 {
-   string word1;
-   string word2;
-   bool valido;
+  string word1;
+  string word2;
+  bool valido;
 
   do
   {
-   valido= true;
-   word1= get_string("Player 1: ");
+  valido = true;
+  word1 = get_string ("Player 1: ");
 
-  for(int i = 0 ; i < strlen(word1) ; i++)
-
-    if(!isalpha(word1[i]))
+  for ( int i = 0 ; i < strlen(word1) ; i++)
+{
+    if (!isalpha(word1[i]))
     {
-        valido= false;
-        break;
+
+    valido = false;
+    break;
     }
-
-  }
-  while(!valido);
-
-  do
-  {
-    valido= true;
-    word2= get_string("Player 2: ");
-
-    for (int j = 0 ; j < strlen(word2) ; j++)
-
-    if(!isalpha(word2[j]))
-    {
-        valido= false;
-        break;
-
-    }
-
-  }
-  while(!valido);
-
-
-  int compute_score(string word1)
-  {
-    int score= 0
-  }
 }
 
+  }
+  while(!valido);
+
+  do
+  {
+    valido = true;
+    word2  = get_string("Player 2: ");
+
+    for(int i = 0 ; i < strlen(word2) ; i++)
+    {
+      if(!isalpha(word2[i]))
+      {
+        valido = false;
+        break;
+      }
+    }
+
+  }
+  while(!valido);
+
+  int score1 = compute_score(word1);
+  int score2 = compute_score(word2);
+
+  if ( score1 > score2)
+{
+   printf("Player 1 venceu\n");
+}
+
+else if(score2 > score1)
+  {
+     printf("Player 2 venceu\n");
+  }
+
+  else
+  {
+    printf("Tie\n");
+  }
+
+  int compute_score(string word);
+
+  for (int i = 0 ; i < strlen(word) ; i++)
+
+  
+
+
+ }
