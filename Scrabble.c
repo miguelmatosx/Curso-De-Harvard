c9028332053c75174ed69f7eb8d7d7ae723f02cd
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-01-30 15:20:09 +0000

    Fri, Jan 30, 2026, 11:20 AM -04:00

--- Scrabble.c
+++ Scrabble.c
@@ -10,8 +10,8 @@ int compute_score(string word);
 
 int main(void)
 {
-  string word1;
-  string word2;
+  string word1 = get_string ("Player 1: ");
+  string word2 = get_string ("Player 2: ");
 
 
   int score1 = compute_score(word1);
