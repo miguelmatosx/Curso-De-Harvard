c883a9487cbe6a3346f23554db066a42072ce24e
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-02 19:20:30 +0000

    Mon, Feb 2, 2026, 3:20 PM -04:00

--- Grade.c
+++ Grade.c
@@ -58,7 +58,21 @@ S = (float) frase/palavras  * 100;
 
  int Grade = round(indice) ;
 
- printf("Grade: %i\n" , Grade);
+ if (Grade < 1)
+ {
+  printf("Before grade 1\n");
+ }
+
+ else if (Grade >= 16)
+ {
+    printf("Grade 16+\n");
+ }
+
+ else
+ {
+   printf("Grade %i\n" , Grade);
+ }
+
 
 
 
