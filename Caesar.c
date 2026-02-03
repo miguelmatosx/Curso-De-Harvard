80394db66eaa2d4df80edd00e03785554e8a4b70
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-03 19:56:00 +0000

    Tue, Feb 3, 2026, 3:56 PM -04:00

--- Caesar.c
+++ Caesar.c
@@ -2,6 +2,7 @@
 #include <cs50.h>
 #include <math.h>
 #include <ctype.h>
+#include <string.h>
 
 int main( int argc , string argv[])
 {
@@ -24,15 +25,16 @@ int main( int argc , string argv[])
    string plaintext = get_string("Qual o texto: ");
 
    for( int i = 0 ; i < strlen(plaintext) ; i++)
-   {
+ {
 
       char c = plaintext[i];
 
    if(isupper(c))
-      {
-         int posicao = c - 'A';
-      }
-      int nova_posicao = (posicao + key) % 26;
+   {
+       int posicao = c - 'A';
+       int nova_posicao = (posicao + key) % 26;
+       char nova_letra = nova_posicao + 'A' ;
+       printf("%c" , nova_letra);
    }
-
+}
 }
