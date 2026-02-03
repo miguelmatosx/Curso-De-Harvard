8a9456a837d9c43b9378e8bae643cf8a6fd8c84f
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-03 18:30:43 +0000

    Tue, Feb 3, 2026, 2:30 PM -04:00

---
+++ Caesar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+#include <ctype.h>
+
+int main( int argc , string argv[])
+{
+   if(argc != 2)
+   {
+    printf("Usage : ./Caesar key\n");
+      return 1;
+   }
+   for(int i = 0 ; argv[1][i] !='\0' ; i++)
+   {
+      if(!isdigit(argv[1][i]))
+      {
+        printf("Usage: ./Caesar key\n");
+        return 1 ;
+
+      }
+
+   }
+
+   string plaintext = get_string("Qual o texto: ");
+}
