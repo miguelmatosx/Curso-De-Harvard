dbfd22ef085ee1ee8dc73247188b75f5d19380b7
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-06 15:35:36 +0000

    Fri, Feb 6, 2026, 11:35 AM -04:00

--- pset3/plurality/plural.c
+++ pset3/plurality/plural.c
@@ -18,5 +18,28 @@ int candidate_count ;
 bool vote (string name);
 void print_winner(void);
 
+int main (int argc , string argv[])
+
+if (argc < 2)
+{
+  printf("Usage plurality [candidate]\n");
+  return 1;
+}
+
+candidate_count = argc - 1 ;
+
+if ( candidate_count > MAX)
+{
+    printf ("Maximum number of candidaters is %i\n" , MAX);
+    return 2;
+}
+
+for (int i = 0 ; i < candidate_count ; i++)
+{
+    candidates[i].names = argv[i + 1];
+    candidates[i].votes = 0;
+}
+
+
 
 
