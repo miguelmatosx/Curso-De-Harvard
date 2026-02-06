5a564f0313a5541e74644591b11e2807f8cdf865
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-06 14:08:18 +0000

    Fri, Feb 6, 2026, 10:08 AM -04:00

--- pset3/plurality/plural.c
+++ pset3/plurality/plural.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
-#include <cs50>
+#include <cs50.h>
 #include <string.h>
+
+#define MAX 9
+
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate ;
+
+candidate candidates[MAX];
+
+int candidate_count ;
+
+bool vote (string name);
+
