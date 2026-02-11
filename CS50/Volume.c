22fd107460d51cc7811a6ba45c658d2e36b0c6b1
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-11 15:30:24 +0000

    Wed, Feb 11, 2026, 11:30 AM -04:00

---
+++ CS50/Volume.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const int HEADER_SIZE = 44;
+
+int main (int argc[] , char *argv[] )
+{
+   if (argc != 4)
+   {
+      printf("Usage: ./volume input.wav output.wav factor\n") ;
+     return 1;
+   }
+
+  FILE *output = fopen (argv[1] , "r");
+  if (output == NULL)
+  {
+    printf ("Could not open the file.\n");
+    return 1;
+  }
+
+  FILE *input = fopen (argv[2] , "w"):
+  if (output== NULL)
+  {
+    printf("Could not open the file.\n");
+    return 1
+
+  }
+
+
+
+
+
+
+}
+
