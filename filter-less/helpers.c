6962e3faf087241d3b7f44e96968ff3e5fdf9092
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-12 10:59:29 +0000

    Thu, Feb 12, 2026, 6:59 AM -04:00

---
+++ filter-less/helpers.c
@@ -0,0 +1,38 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for( int i = 0 ; i < height ; i++)
+    {
+       for( int j = 0 ; j < width ; j++)
+       {
+         float avg = (image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0;
+
+         image[i][j].rgbtRed = round(avg);
+         image[i][j].rgbtBlue = round(avg);
+         image[i][j].rgbtGreen = round(avg);
+
+       }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
