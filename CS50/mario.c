a658808ee7ba7bb67721f6c8c632424a6f2088dd
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-16 21:14:21 +0000

    Mon, Feb 16, 2026, 5:14 PM -04:00

--- CS50/mario.c
+++ CS50/mario.c
@@ -3,16 +3,28 @@
 
 int main (void)
 {
-    int height
+    int height;
 
     do
     {
-        height = get_int ("Height: "):
+        height = get_int ("Height: ");
     }
-    while( height > 1 || height < 8);
 
-    for(int i = 0  ; i < height  ; i++)
+    while( height < 1 || height > 8);
+
+    for (int i = 0 ; i < height  ; i++)
     {
         for (int j = 0 ; j < i ; j++)
+       {
+         printf(" ");
+       }
+
+  for( int k = 0 ; k <= i ; k++)
+ {
+    printf("#");
+ }
+ printf("\n");
+
     }
+
 }
