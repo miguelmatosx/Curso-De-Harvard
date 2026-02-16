6f72f4b82c5986ef9a324c922e52fc8bd7847f16
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-16 16:51:57 +0000

    Mon, Feb 16, 2026, 12:51 PM -04:00

--- CS50/mario.c
+++ CS50/mario.c
@@ -13,7 +13,7 @@ void draw(int height)
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             printf("#");
         }
