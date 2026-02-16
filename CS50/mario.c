5128b46fa91d833a8aa44d427139b37960dcc4c6
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-16 16:50:43 +0000

    Mon, Feb 16, 2026, 12:50 PM -04:00

--- CS50/mario.c
+++ CS50/mario.c
@@ -1,23 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void draw (int h );
+void draw(int h);
 
-int main (void)
+int main(void)
 {
-    int height = get_int ("Altura : ");
+    int height = get_int("Altura: ");
     draw(height);
-}
+} // <-- fecha main
 
-void draw ( int height)
+void draw(int height)
 {
-    for (int i = 0 ; i < height ; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0 ; j < i ; i++)
+        for (int j = 0; j < i; j++)
         {
-           printf ("#");
+            printf("#");
         }
-        printf ("\n");
+        printf("\n");
     }
 }
 
