67a3ab8d8ea3e7d755b16ad21c55075d3069de4c
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-19 13:48:01 +0000

    Thu, Feb 19, 2026, 9:48 AM -04:00

--- CS50/MarioMore.c
+++ CS50/MarioMore.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-int main (void)
+int main(void)
 {
     int height;
 
@@ -9,29 +9,27 @@ int main (void)
     {
         height = get_int("Height: ");
     }
-    while( height < 1 || height > 8);
+    while (height < 1 || height > 8);
 
-    for (int i = 0 ; i < height ; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0 ; j < height - i - 1 ; j++)
+        for (int j = 0; j < height - i - 1; j++)
         {
             printf(" ");
         }
 
-
-        for (int j = 0 ; j <= i ; j++)
-        {
-           printf("#");
-        }
-
-
-        printf("  ");
-
-
-        for( int j = 0 ; j <= i ; j++)
+        for (int j = 0; j <= i; j++)
         {
             printf("#");
         }
+
+        printf("  ");
+
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
         printf("\n");
     }
 }
