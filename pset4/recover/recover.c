56b8a8c6a51a32a97d1efc5851b91dca32fbd46a
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-20 12:56:17 +0000

    Fri, Feb 20, 2026, 8:56 AM -04:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+   if (argc != 2 )
+   {
+    printf("Usage:./recover image\n");
+    return 1 ;
+   }
+
+   FILE *file = fopen(argv[1] , r);
+  if (file == NULL)
+  {
+    printf("Could not open\n");
+    return 1;
+  }
+
+  unsigned char buffer[512]
+  FILE *img = NULL;
+  int jpg_count = 0;
+
+  while( fread (buffer , 1 , 512 , file) == 512)
+  {
+    if (buffer[0] == 0xff &&
+    buffer[1] == 0xd8 &&
+    buffer[2] == 0xff &&
+    (buffer[3] & 0xf0) == 0xe0)
+
+  }
+
+
+
 }
