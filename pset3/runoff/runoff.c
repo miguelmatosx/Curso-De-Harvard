4fdc5e014ee963d23ba1cc34c3cdf1d88141d0e5
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-20 12:19:23 +0000

    Fri, Feb 20, 2026, 8:19 AM -04:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
 
     for (int i = 0 ; i < candidate_count ; i++)
     {
-        for(strcmp( candidates[i].name , name) == 0 );
+        for(strcmp(candidates[i].name , name) == 0 );
         {
             preferences[voter][rank] = 1;
             return true;
