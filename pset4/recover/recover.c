aeec2c3f48e6d70e769181ef7872dccec3921104
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-20 13:06:43 +0000

    Fri, Feb 20, 2026, 9:06 AM -04:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -22,19 +22,17 @@ int main(int argc, char *argv[])
 
   while( fread (buffer , 1 , 512 , file) == 512)
   {
-    if
-    {
-    (buffer[0] == 0xff &&
+    if (buffer[0] == 0xff &&
     buffer[1] == 0xd8 &&
     buffer[2] == 0xff &&
     (buffer[3] & 0xf0) == 0xe0)
 
      if (img !== NULL)
      {
-        
+       fclose (img);
      }
 
-    }
+}
 
 
 
