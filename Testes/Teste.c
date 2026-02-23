60be6af630cc13aac28deaae64311b67ae33f0b9
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-02-23 12:42:51 +0000

    Mon, Feb 23, 2026, 8:42 AM -04:00

--- Testes/Teste.c
+++ Testes/Teste.c
@@ -1,17 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+ node;
 
 int main (void)
 {
-    int list[3];
+    node *list == NULL;
 
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    for( int i = 0 ; i < 3 ; i++)
+    for(int i = 0 ; i < 3 ; i++)
     {
-        printf("%i\n" , list[i]);
+        node *n = malloc(sizeof(node));
+        if( n == NULL)
+        {
+            return 1;
+        }
+        n ->number = get_int("Number: ");
+        n ->next = NULL;
     }
+
+
 
 }
