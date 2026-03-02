ecdf915d820b37956da023b1c73d2930a105c7e9
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-03-02 21:31:09 +0000

    Mon, Mar 2, 2026, 5:31 PM -04:00

--- pset6/readability.py
+++ pset6/readability.py
@@ -9,7 +9,7 @@ frases = 0
 for ch in texto:
     if ch.isalpha(): letras +=1
     elif ch.isspace(): palavras +=1
-    elif ch in ".!?'": frases += 1
+    elif ch in ".!?": frases += 1
 
 L = letras / palavras * 100
 S = frases / palavras * 100
