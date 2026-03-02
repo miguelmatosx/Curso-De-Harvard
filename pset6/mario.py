2cc4a6a91473d80fd47124309b098a8134e505f3
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-03-02 13:34:07 +0000

    Mon, Mar 2, 2026, 9:34 AM -04:00

--- pset6/mario.py
+++ pset6/mario.py
@@ -1,8 +1,9 @@
 Height = int(input("Altura:  "))
 if Height < 1 or Height > 8:
     print("inválido")
+    return
 else:
- for i in range(Height):
+    for i in range(Height):
     space = " " * (Height - i - 1)
     hashes= "#" * (i + 1 )
     print(space + hashes)
