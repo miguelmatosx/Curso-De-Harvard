b22708419400dc9b9d8c895ac5c5d9e72b550eb7
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-03-03 14:23:41 +0000

    Tue, Mar 3, 2026, 10:23 AM -04:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if sys.argv != 3:
+    if sys.argv != 4:
         print("Inválido")
 
     # TODO: Read database file into a variable
