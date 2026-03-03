8e963d336ac34bfbcd7b43942c8949f9925dacde
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-03-03 18:32:24 +0000

    Tue, Mar 3, 2026, 2:32 PM -04:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -13,10 +13,10 @@ def main():
 
 
     # TODO: Read DNA sequence file into a variable
-    with open(sys.argv[4] , 'r') as sequences
+    with open(sys.argv[4] , 'r') as sequences:
 
     # TODO: Find longest match of each STR in DNA sequence
-    
+
 
 
     # TODO: Check database for matching profiles
