94362bd683f24fa4693c682dc876b17cabc8f766
miguelmcmatos-lab <miguelmcmatos@gmail.com>
2026-03-04 19:08:26 +0000

    Wed, Mar 4, 2026, 3:08 PM -04:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -63,41 +63,15 @@ def main():
 
 
 def longest_match(sequence, subsequence):
-    """Returns length of longest run of subsequence in sequence."""
-
- # Initialize variables
-    longest_run = 0
-    subsequence_length = len(subsequence)
-    sequence_length = len(sequence)
-
-    # Check each character in sequence for most consecutive runs of subsequence
-    for i in range(sequence_length):
-
-        # Initialize count of consecutive runs
-        count = 0
-
-        # Check for a subsequence match in a "substring" (a subset of characters) within sequence
-        # If a match, move substring to next potential match in sequence
-        # Continue moving substring and checking for matches until out of consecutive matches
-        while True:
-
-            # Adjust substring start and end
-            start = i + count * subsequence_length
-            end = start + subsequence_length
-
-            # If there is a match in the substring
-            if sequence[start:end] == subsequence:
-                count += 1
-
-            # If there is no match in the substring
-            else:
-                break
-
-        # Update most consecutive matches found
-        longest_run = max(longest_run, count)
-
-    # After checking for runs at each character in sequence, return longest run found
-    return longest_run
-
+  def longest_match(sequence, subsequence):
+    length = len(subsequence)           # linha 2 do diagrama
+    longest = 0                         # linha 4
+    for i in range(len(sequence)):      # linha 6
+        count = 0                       # linha 8
+        while i + count * length <= len(sequence) and sequence[i:i+length] == subsequence:
+            count += 1                  # linha 12
+        if count > longest:             # linha 14
+            longest = count             # linha 15
+    return longest 
 
 main()
